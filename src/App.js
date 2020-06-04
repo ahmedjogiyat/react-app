2426c83d8400fe258f51690f1bd864766bbecec5
ahmed jogiyat <ahmedjogiyat123@gmail.com>
2020-06-04 22:48:02 +0500

    initialize project using react

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div >
+      <h1>My First React App</h1>
+      <h2>with the help of BootCamp 2020 </h2>
+    </div>
+  );
+}
+
+export default App;
