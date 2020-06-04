dab7c3846cf0e1bd1509db6c7ffd865edea370c6
ahmed jogiyat <ahmedjogiyat123@gmail.com>
2020-06-05 00:13:08 +0500

    add.

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div >
       <h1>My First React App</h1>
       <h2>with the help of BootCamp 2020 </h2>
-      <h3>DATE 6/4/202000000</h3>
+      <h3>DATE 6/4/202000</h3>
       
     </div>
   );
